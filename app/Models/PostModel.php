c81f06f5f360e59f5f384ea5f5a53ee5d41e61b3
setiawan <setiawand000@gmail.com>
2022-04-13 12:17:39 +0700

    menambah file ke git

---
+++ app/Models/PostModel.php
@@ -0,0 +1,16 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+class PostModel extends Model
+{
+    protected $table = 'posts';
+
+    protected $allowedFields = [
+        'title',
+        'content',
+    ];
+}
+
+
+
+?>
